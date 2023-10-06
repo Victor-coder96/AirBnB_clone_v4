7eb724318e2fb790f8c4805e8061795ecadc5792
Wakadaisho <paulwahome9@gmail.com>
2023-10-06 16:36:26 +0300

    Add places search, dynamic states and cities filter, reviews

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+  let amenities = [];
+
+  $('div.amenities li input[type=checkbox]').change(function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      checkedAmenities[id] = name;
+    } else {
+      delete checkedAmenities[id];
+    }
+    $('div.amenities h4').text(Object.values(checkedAmenities).join(', '));
+    amenities = Object.keys(checkedAmenities);
+  });
+
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('header#api_status').removeClass('available');
+    }
+  });
+
+  makePlacesRequst();
+
+  $('button').click(function () {
+    const checkedAmenities = [];
+    $('div.amenities li input[type=checkbox]').change(function () {
+      const id = $(this).attr('data-id');
+      if ($(this).is(':checked')) {
+        checkedAmenities.push(id);
+      }
+    });
+    makePlacesRequst({ amenities });
+  });
+});
+
+function makePlacesRequst (post = {}) {
+  $.post({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    dataType: 'json',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify(post)
+  }, function (data) {
+    $('section.places').empty();
+    const html = data.reduce((articles, place) => {
+      return (articles + `
+        <article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+          <div class="user"></div>
+          <div class="description">${place.description}</div>
+        </article>`);
+    }, '');
+    $('section.places').append(html);
+  });
+}
